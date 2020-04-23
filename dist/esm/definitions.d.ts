c1c5964cefd4f9a50d38bdf9f983f918dfd179db
Jay Ordway <jay.ordway@bcbsma.com>
2020-04-23 05:31:42 -0500

    fix for class name

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,32 @@
+declare module "@capacitor/core" {
+    interface PluginRegistry {
+        AppPermissionsPlugin: AppPermissionsPlugin;
+    }
+}
+export interface Plugin {
+    addListener(eventName: string, listenerFunc: Function): PluginListenerHandle;
+    requestPermissions?: () => Promise<PermissionsRequestResult>;
+}
+export interface PluginListenerHandle {
+    remove: () => void;
+}
+export interface PermissionsRequestResult {
+    results: any[];
+}
+export interface AppPermissionsPlugin extends Plugin {
+    query(options: PermissionsOptions): Promise<PermissionResult>;
+}
+export declare enum PermissionType {
+    Camera = "camera",
+    Photos = "photos",
+    Geolocation = "geolocation",
+    Notifications = "notifications",
+    ClipboardRead = "clipboard-read",
+    ClipboardWrite = "clipboard-write"
+}
+export interface PermissionsOptions {
+    name: PermissionType;
+}
+export interface PermissionResult {
+    state: 'granted' | 'denied' | 'prompt';
+}
