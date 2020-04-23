09ec7b95f4df53c3a9fd2657e3a4682cf5dc554d
Jay Ordway <jay.ordway@bcbsma.com>
2020-04-23 05:49:29 -0500

    fix

--- src/web.ts
+++ src/web.ts
@@ -10,6 +10,23 @@ export class AppPermissionsPluginWeb extends WebPlugin implements PermissionsPlu
                     });
                   }
 
+   async query(options: PermissionsOptions): Promise<PermissionResult> {
+       const navigator = window.navigator as any;
+
+       if (!navigator.permissions) {
+         return Promise.reject('This browser does not support the Permissions API');
+       }
+
+       // Photos isn't supported in the web but it's equivalent to the camera permission
+       // since the prompt lets you pick from an album
+       const name = options.name === PermissionType.Photos ? 'camera' : options.name;
+
+       const ret = await navigator.permissions.query({ name });
+
+       return {
+         state: ret.state
+       };
+     }
 }
 
 const AppPermissionsPlugin = new AppPermissionsPluginWeb();
