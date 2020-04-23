8514cee1e94014fe60e108f3d22d5d0aa6146dab
Jay Ordway <jay.ordway@bcbsma.com>
2020-04-23 06:46:46 -0500

    fix: adding import

--- dist/esm/web.js
+++ dist/esm/web.js
@@ -29,7 +29,7 @@ export class AppPermissionsPluginWeb extends WebPlugin {
             if (!navigator.permissions) {
                 return Promise.reject('This browser does not support the Permissions API');
             }
-            const name = options.name;
+            const name = options.name === PermissionType.Photos ? 'camera' : options.name;
             const ret = yield navigator.permissions.query({ name });
             return {
                 state: ret.state
