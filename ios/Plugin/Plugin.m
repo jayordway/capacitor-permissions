8e9449ec246f30629aaaa9b41b2ada20987ca70a
Jay Ordway <jay.ordway@bcbsma.com>
2020-04-24 08:33:17 -0500

    updating ios files

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,6 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(AppPermissionsPlugin, "AppPermissionsPlugin",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(request, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(query, CAPPluginReturnPromise);
 )
