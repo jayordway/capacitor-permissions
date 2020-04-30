33d1d43f3d63b33ed3410289e67a6f77b2143c71
Jay Ordway <jay.ordway@bcbsma.com>
2020-04-29 23:09:18 -0500

    fixes for android

--- android/src/main/java/com/capacitor/permissions/AppPermissionsPlugin.java
+++ android/src/main/java/com/capacitor/permissions/AppPermissionsPlugin.java
@@ -6,10 +6,12 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 
-
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
@@ -17,22 +19,52 @@ import androidx.core.content.ContextCompat;
 @NativePlugin()
 public class AppPermissionsPlugin extends Plugin {
 
+    static final int REQUEST_CAMERA = 20120;
+    static final int REQUEST_READ_EXTERNAL_STORAGE = 20130;
+    static final int REQUEST_WRITE_EXTERNAL_STORAGE = 20140;
+    static final int REQUEST_COARSE_LOCATION = 20150;
+    static final int REQUEST_NOTIFICATION_POLICY = 20160;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @PluginMethod
-    public void request(PluginCall call){
-        JSObject ret = new JSObject();
+    public void request(PluginCall call) {
+        String name = call.getString("permission");
 
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-            ret.put("state", "GRANTED");
+        switch (name) {
+            case "camera":
+                requestPermissions(call, Manifest.permission.CAMERA, REQUEST_CAMERA);
+                break;
+            case "photos":
+                requestPermissions(call, Manifest.permission.READ_EXTERNAL_STORAGE, REQUEST_READ_EXTERNAL_STORAGE);
+                break;
+            case "geolocation":
+                requestPermissions(call, Manifest.permission.ACCESS_COARSE_LOCATION, REQUEST_COARSE_LOCATION);
+                break;
+            case "notifications":
+                requestPermissions(call, Manifest.permission.ACCESS_NOTIFICATION_POLICY, REQUEST_NOTIFICATION_POLICY);
+                break;
+            case "file-write":
+                requestPermissions(call, Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_WRITE_EXTERNAL_STORAGE);
+                break;
+            default:
+                call.reject("Unknown permission type");
         }
-        else
-        {
-            ret.put("state", "ALREADY GRANTED");
+
+    }
+
+    private void requestPermissions(PluginCall call, String permission, int permissionConstant) {
+        JSObject ret = new JSObject();
+        try {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, permissionConstant);
+            ret.put("state", "granted");
+        } catch (Error error){
+            ret.put("state", "denied");
         }
 
         call.resolve(ret);
     }
+
 
     @PluginMethod
     public void query(PluginCall call) {
@@ -50,10 +82,6 @@ public class AppPermissionsPlugin extends Plugin {
                 break;
             case "notifications":
                 checkNotifications(call);
-                break;
-            case "clipboard-read":
-            case "clipboard-write":
-                checkClipboard(call);
                 break;
             case "file-write":
                 checkFileWrite(call);
@@ -75,7 +103,7 @@ public class AppPermissionsPlugin extends Plugin {
         call.resolve(ret);
     }
 
-    private void checkFileWrite(PluginCall call){
+    private void checkFileWrite(PluginCall call) {
         checkPerm(Manifest.permission.WRITE_EXTERNAL_STORAGE, call);
     }
 
@@ -98,9 +126,4 @@ public class AppPermissionsPlugin extends Plugin {
         call.resolve(ret);
     }
 
-    private void checkClipboard(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("state", "granted");
-        call.resolve(ret);
-    }
 }
